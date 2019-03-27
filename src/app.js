193d5d398ac72f8c23a87d7d7438cbed1cebef46
adrian <adrian.misko5@gmail.com>
2019-03-28 00:38:48 +0100

    Stop saving state in local storage
    We're now sending auth token with some requests. Unfortunately we can't keep it in the local storage due to XSS, neither can we store it in cookie (CRSF), because back-end server doesn't have any CRSF protection

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 export const dva = {
   config: {
-    initialState: JSON.parse(window.sessionStorage.getItem('state')),
+    // initialState: JSON.parse(window.sessionStorage.getItem('state')),
     onError(err) {
       err.preventDefault();
       console.error(err.message);
     },
     onStateChange(state) {
-      window.sessionStorage.setItem('state', JSON.stringify(state));
+      // window.sessionStorage.setItem('state', JSON.stringify(state));
     },
   },
 };
